cf3b891ae9d5bf0d2c8a9ce26c9876cd065bed63
Luis <Luis@DESKTOP-0JM9936>
2023-05-17 09:20:09 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoSen/Equipo.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ProyectoSen
+{
+    partial class Equipo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Equipo));
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvEquipo = new System.Windows.Forms.DataGridView();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.cmbTipoE = new System.Windows.Forms.ComboBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEquipo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(87, 91);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(132, 22);
+            this.txtId.TabIndex = 6;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtMarca.Location = new System.Drawing.Point(376, 209);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(132, 22);
+            this.txtMarca.TabIndex = 8;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.BackColor = System.Drawing.Color.White;
+            this.txtDescripcion.Location = new System.Drawing.Point(29, 318);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(525, 221);
+            this.txtDescripcion.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "N° Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 174);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "TIPO DE EQUIPO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Marca";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 282);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Descripcion de Falla";
+            // 
+            // dgvEquipo
+            // 
+            this.dgvEquipo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEquipo.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvEquipo.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvEquipo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEquipo.Location = new System.Drawing.Point(587, 135);
+            this.dgvEquipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvEquipo.Name = "dgvEquipo";
+            this.dgvEquipo.RowHeadersWidth = 51;
+            this.dgvEquipo.Size = new System.Drawing.Size(375, 201);
+            this.dgvEquipo.TabIndex = 14;
+            this.dgvEquipo.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnModificar.Location = new System.Drawing.Point(409, 52);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(100, 36);
+            this.btnModificar.TabIndex = 15;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.Location = new System.Drawing.Point(545, 52);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(148, 34);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "GUARDAR";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClear.Location = new System.Drawing.Point(713, 52);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(116, 34);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "ELIMINAR";
+            this.btnClear.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnVolver.Location = new System.Drawing.Point(900, 52);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(100, 36);
+            this.btnVolver.TabIndex = 18;
+            this.btnVolver.Text = "VOLVER";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // cmbTipoE
+            // 
+            this.cmbTipoE.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmbTipoE.FormattingEnabled = true;
+            this.cmbTipoE.Items.AddRange(new object[] {
+            "LAPTOP",
+            "IMPRESORA",
+            "CPU"});
+            this.cmbTipoE.Location = new System.Drawing.Point(59, 208);
+            this.cmbTipoE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbTipoE.Name = "cmbTipoE";
+            this.cmbTipoE.Size = new System.Drawing.Size(160, 24);
+            this.cmbTipoE.TabIndex = 19;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.Location = new System.Drawing.Point(954, 505);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 36);
+            this.btnReset.TabIndex = 20;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Equipo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.cmbTipoE);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.dgvEquipo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtId);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Equipo";
+            this.Text = "Equipo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEquipo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvEquipo;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.ComboBox cmbTipoE;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ ProyectoSen/Principal.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ProyectoSen
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPrincipal = new System.Windows.Forms.DataGridView();
+            this.btnTecnico = new System.Windows.Forms.Button();
+            this.btnEquipo = new System.Windows.Forms.Button();
+            this.btnCliente = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.cmbCrear = new System.Windows.Forms.ComboBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPrincipal
+            // 
+            this.dgvPrincipal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPrincipal.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dgvPrincipal.BackgroundColor = System.Drawing.Color.White;
+            this.dgvPrincipal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrincipal.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgvPrincipal.Location = new System.Drawing.Point(35, 220);
+            this.dgvPrincipal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvPrincipal.Name = "dgvPrincipal";
+            this.dgvPrincipal.ReadOnly = true;
+            this.dgvPrincipal.RowHeadersWidth = 51;
+            this.dgvPrincipal.Size = new System.Drawing.Size(776, 286);
+            this.dgvPrincipal.TabIndex = 0;
+            // 
+            // btnTecnico
+            // 
+            this.btnTecnico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnTecnico.Location = new System.Drawing.Point(60, 89);
+            this.btnTecnico.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTecnico.Name = "btnTecnico";
+            this.btnTecnico.Size = new System.Drawing.Size(100, 28);
+            this.btnTecnico.TabIndex = 1;
+            this.btnTecnico.Text = "TECNICO";
+            this.btnTecnico.UseVisualStyleBackColor = false;
+            this.btnTecnico.Click += new System.EventHandler(this.btnTecnico_Click);
+            // 
+            // btnEquipo
+            // 
+            this.btnEquipo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEquipo.Location = new System.Drawing.Point(353, 89);
+            this.btnEquipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEquipo.Name = "btnEquipo";
+            this.btnEquipo.Size = new System.Drawing.Size(100, 28);
+            this.btnEquipo.TabIndex = 2;
+            this.btnEquipo.Text = "EQUIPO";
+            this.btnEquipo.UseVisualStyleBackColor = false;
+            this.btnEquipo.Click += new System.EventHandler(this.btnEquipo_Click);
+            // 
+            // btnCliente
+            // 
+            this.btnCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCliente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCliente.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCliente.Location = new System.Drawing.Point(671, 89);
+            this.btnCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCliente.Name = "btnCliente";
+            this.btnCliente.Size = new System.Drawing.Size(100, 28);
+            this.btnCliente.TabIndex = 3;
+            this.btnCliente.Text = "CLIENTE";
+            this.btnCliente.UseVisualStyleBackColor = false;
+            this.btnCliente.Click += new System.EventHandler(this.btnCliente_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCrear.Location = new System.Drawing.Point(883, 393);
+            this.btnCrear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(100, 28);
+            this.btnCrear.TabIndex = 4;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // cmbCrear
+            // 
+            this.cmbCrear.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cmbCrear.FormattingEnabled = true;
+            this.cmbCrear.Items.AddRange(new object[] {
+            "TECNICO",
+            "CLIENTE",
+            "EQUIPO"});
+            this.cmbCrear.Location = new System.Drawing.Point(860, 303);
+            this.cmbCrear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbCrear.Name = "cmbCrear";
+            this.cmbCrear.Size = new System.Drawing.Size(160, 24);
+            this.cmbCrear.TabIndex = 16;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(879, 354);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 16);
+            this.lblMessage.TabIndex = 17;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.cmbCrear);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnCliente);
+            this.Controls.Add(this.btnEquipo);
+            this.Controls.Add(this.btnTecnico);
+            this.Controls.Add(this.dgvPrincipal);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPrincipal;
+        private System.Windows.Forms.Button btnTecnico;
+        private System.Windows.Forms.Button btnEquipo;
+        private System.Windows.Forms.Button btnCliente;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.ComboBox cmbCrear;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
